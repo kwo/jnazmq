7b5eea715d5a4212cd5c969016e8f5cbfdc29b9c
kwo <karl@ostendorf.com>
2011-05-09 17:57:15 +0200

    removed error enum

--- src/main/java/org/zeromq/ZmqException.java
+++ src/main/java/org/zeromq/ZmqException.java
@@ -2,36 +2,6 @@ package org.zeromq;
 
 public class ZmqException extends RuntimeException {
 
-	public static enum Code {
-
-		EADDRINUSE(ZmqLibrary.EADDRINUSE),
-		EADDRNOTAVAIL(ZmqLibrary.EADDRNOTAVAIL),
-		ECONNREFUSED(ZmqLibrary.ECONNREFUSED),
-		EFSM(ZmqLibrary.EFSM),
-		EINPROGRESS(ZmqLibrary.EINPROGRESS),
-		EMTHREAD(ZmqLibrary.EMTHREAD),
-		ENETDOWN(ZmqLibrary.ENETDOWN),
-		ENOBUFS(ZmqLibrary.ENOBUFS),
-		ENOCOMPATPROTO(ZmqLibrary.ENOCOMPATPROTO),
-		ENOTSUP(ZmqLibrary.ENOTSUP),
-		EPROTONOSUPPORT(ZmqLibrary.EPROTONOSUPPORT),
-		ETERM(ZmqLibrary.ETERM);
-
-		public static Code findByCode(final int code) {
-			for (final Code x : values())
-				if (code == x.code)
-					return x;
-			throw new IllegalArgumentException("Unknown " + Code.class.getSimpleName() + ": " + code);
-		}
-
-		public final int code;
-
-		Code(final int code) {
-			this.code = code;
-		}
-
-	}
-
 	private static final long serialVersionUID = 1L;
 	private final int code;
 
